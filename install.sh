29e48be5818bb918c436312ddc9ec88d0637df0d
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-11-04 14:04:35 +0900

    zshrc整理, rbenv設定コメントアウト, エイリアス若干追加

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 # bundle をインストール
 git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/vimfiles/bundle/neobundle.vim
+
+# percol
+# curl -kL https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python
+# sudo pip install percol
+
+# change shell
+# chsh -s /usr/bin/zsh
