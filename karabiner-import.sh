68f949fdb43e089b01cf7f6107d97650637411af
Kazumasa KUMAMOTOF29 <kumamotone@gmail.com>
2015-12-21 06:54:25 +0900

    karabiner-import.sh の追加

---
+++ karabiner-import.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.jis_eisuu2commandL_eisuu 1
+/bin/echo -n .
+$cli set remap.redo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.jis_yen2backslash 1
+/bin/echo -n .
+$cli set option.emacsmode_controlH 1
+/bin/echo -n .
+$cli set option.emacsmode_controlM 1
+/bin/echo -n .
+$cli set remap.jis_shift0_to_backslash 1
+/bin/echo -n .
+$cli set repeat.wait 30
+/bin/echo -n .
+$cli set remap.pcstyle_close 1
+/bin/echo -n .
+$cli set remap.new_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.pointing_controlLeftclick2commandLeftclick 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF_ex 1
+/bin/echo -n .
+$cli set remap.pcstyle_ctrl_alt_del 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF_nomodifiers 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.save_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_new_tab 1
+/bin/echo -n .
+$cli set repeat.initial_wait 200
+/bin/echo -n .
+/bin/echo
